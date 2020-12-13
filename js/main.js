bdb52c28423201d8e897ae4db39bcbb04bbcfeeb
ダウスケ <ksye616@gmail.com>
2020-12-13 12:50:14 +0900

    no_reload&sound

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ $(() => {
     bingo = [],
     status = true,
     roulette,
+    sound,
     random,
     number,
     result,
@@ -17,6 +18,10 @@ $(() => {
     $number.append($("<li>").text(("0" + i).slice(-2)));
   }
 
+  //音声ファイル初期化・読み込み
+  const audio_drumroll = new Audio("../sounds/drum-roll1.mp3");
+  const audio_rollfinish = new Audio("../sounds/roll-finish1.mp3");
+
   $("#button").on("click", function(){
     if(status) {
       status = false;
@@ -26,9 +31,18 @@ $(() => {
         random = Math.floor(Math.random() * bingo.length);
         number = bingo[random];
         $result.text(number);
+        audio_drumroll.play();  //ドラムロールスタート
       }, 10);
+
+      audio_rollfinish.pause();
+      audio_rollfinish.currentTime = 0;
     } else {
       status = true;
+      audio_drumroll.pause();
+      audio_drumroll.currentTime = 0;     //ドラムロールストップ
+      audio_rollfinish.currentTime = 0;   //ストップ時効果音
+      audio_rollfinish.play();
+
       $(this).text("START");
 
       clearInterval(roulette);
